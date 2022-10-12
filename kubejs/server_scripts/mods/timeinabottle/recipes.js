495b13bef66405f7772377988bf48b831db01f39
WTD <mattmaryanski@gmail.com>
2022-10-12 12:47:23 -0400

    temp TIAB recipe to fix error

--- kubejs/server_scripts/mods/timeinabottle/recipes.js
+++ kubejs/server_scripts/mods/timeinabottle/recipes.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(event => {
   event.remove({ id: 'tiab:time_in_a_bottle' })
   event.shaped('tiab:time_in_a_bottle', ['UUU', 'DCD', 'LBL'], {
     U: '#forge:ingots/unobtainium',
-    D: 'botania:mana_diamond',
+    D: 'mysticalagriculture:speed_iii_augment',
     L: 'minecraft:lapis_lazuli',
     C: 'productivebees:upgrade_time',
     B: 'minecraft:experience_bottle'
