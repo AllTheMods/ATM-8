e1f014ac6f160c039ff6ba57c247459c79fdb31b
enigmaquip <enigmaquip@users.noreply.github.com>
2022-10-12 14:15:59 -0600

    RS Kube addtions

---
+++ kubejs/client_scripts/jei_hide.js
@@ -0,0 +1,9 @@
+JEIEvents.hideItems(event => {
+    event.hide(/extrastorage:(block|disk|storagepart)_.+/)
+    event.hide(/extrastorage:advanced_(importer|exporter)/)
+    Color.DYE.forEach(color => {
+        ['controller', 'creative_controller', 'grid', 'crafting_grid', 'pattern_grid', 'fluid_grid', 'network_receiver', 'network_transmitter', 'relay', 'detector', 'security_manager', 'wireless_transmitter', 'disk_manipulator', 'crafter', 'crafter_manager', 'crafting_monitor'].forEach(machine => {
+            event.hide(`refinedstorage:${color}_${machine}`)
+        })
+    })
+})
