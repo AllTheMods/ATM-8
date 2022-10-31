8d557a083ed139a4ab894f9b58c8b1f6a1c1b5c2
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2022-10-31 23:02:13 +0800

    Update atm_alloys.js
    Formatting fixes

--- kubejs/server_scripts/modpack/atm_alloys.js
+++ kubejs/server_scripts/modpack/atm_alloys.js
@@ -11,7 +11,7 @@ ServerEvents.recipes(event => {
     ],
     energy: '1000000000',
 	result: Item.of('allthemodium:vibranium_allthemodium_alloy_ingot').toJson()
-  }).id(`kubejs:energizing/allthemodium:vibranium_allthemodium_alloy_ingot`)
+  }).id(`kubejs:energizing/allthemodium_vibranium_allthemodium_alloy_ingot`)
   
   
  // ATM-Vibranium Block
@@ -38,7 +38,7 @@ ServerEvents.recipes(event => {
     ],
     energy: '1000000000',
     result: Item.of('allthemodium:unobtainium_allthemodium_alloy_ingot').toJson()
-  }).id(`kubejs:energizing/allthemodium:unobtainium_allthemodium_alloy_ingot`)
+  }).id(`kubejs:energizing/allthemodium_unobtainium_allthemodium_alloy_ingot`)
   
 // ATM-Unobtainium Block
 /*
@@ -65,7 +65,7 @@ ServerEvents.recipes(event => {
     ],
     energy: '1000000000',
     result: Item.of('allthemodium:unobtainium_vibranium_alloy_ingot').toJson()
-  }).id(`kubejs:energizing/allthemodium:unobtainium_vibranium_alloy_ingot`)
+  }).id(`kubejs:energizing/allthemodium_unobtainium_vibranium_alloy_ingot`)
   
 // Unobtainium-Vibranium Awakened Block
   event.custom({
