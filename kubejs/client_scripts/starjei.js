280bb7c72dd61387aff89734105b8d86c0032bd6
AlfredGG <alfredggtwitch@gmail.com>
2022-11-05 11:21:28 -0500

    ATM8 Star and Alloy Recipes
    - Added a way to make ATM Alloy Tools (special thanks to Enigma)
    - Removed alloyblock file, converged recipes in atm_alloys
    - Finalized ATM Star recipe
    - Brand new Dimensional Seed Recipe using Spirit Engulfing
    - Created recipes for Mini Portals (needed for Seed) in mini_portals file
    - Removed mini_twilight_portal file, moved contents to mini_portals file
    - Finalized most Allthetweak items needed for Star, cleaned up recipes
    - Made custom items searchable within JEI thanks to Enigma (starjei.js)
    - Removed "Cannot be crafted" JEI Tooltip from Star Items
    - The ATM Star is now craftable

---
+++ kubejs/client_scripts/starjei.js
@@ -0,0 +1,17 @@
+// Force Star Items to require NBT
+JEIEvents.subtypes(event => {
+    event.useNBT("allthemodium:unobtainium_vibranium_alloy_block")
+	event.useNBT("mekanism:teleportation_core")
+})
+
+// Add Star Items to JEI
+JEIEvents.addItems(event => {
+	// Draconic Infused Teleportation Core
+	event.add(Item.of('mekanism:teleportation_core', "{HideFlags:1,display:{Name:'[{\"text\":\"Draconic Infused Teleportation Core\",\"italic\":false}]'}}").enchant('unbreaking', 1).toJson())
+	// Ender Infused Teleportation Core
+	event.add(Item.of('mekanism:teleportation_core', "{HideFlags:1,display:{Name:'[{\"text\":\"Ender Infused Teleportation Core\",\"italic\":false}]'}}").enchant('unbreaking', 1).toJson())
+	// Nether Infused Teleportation Core
+	event.add(Item.of('mekanism:teleportation_core', "{HideFlags:1,display:{Name:'[{\"text\":\"Nether Infused Teleportation Core\",\"italic\":false}]'}}").enchant('unbreaking', 1).toJson())
+	//Unobtainium-Vibranium Awakened Block
+	event.add(Item.of('allthemodium:unobtainium_vibranium_alloy_block', "{HideFlags:1,display:{Name:'[{\"text\":\"Awakened Unobtainium-Vibranium Alloy Block\",\"italic\":false}]'}}").enchant('unbreaking', 1).toJson())
+})
