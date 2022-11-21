65e7262a20fe0d0f73c61b8456a127ccf13431a9
enigmaquip <enigmaquip@users.noreply.github.com>
2022-11-21 09:25:15 -0700

    Fix error hiding rings

--- kubejs/client_scripts/jei_hide.js
+++ kubejs/client_scripts/jei_hide.js
@@ -3,7 +3,7 @@ JEIEvents.hideItems(event => {
   event.hide(/rebornstorage:(small|medium|large|larger)_(item|fluid)_disk.*/)
   event.hide(/extrastorage:advanced_(importer|exporter)/)
   event.hide('twilightforest:uncrafting_table')
-  event.hide('angelring:leadstone_angel_ring','angelring:hardened_angel_ring','angelring:reinforced_angel_ring','angelring:resonant_angel_ring')
+  event.hide(['angelring:leadstone_angel_ring', 'angelring:hardened_angel_ring', 'angelring:reinforced_angel_ring', 'angelring:resonant_angel_ring'])
   Color.DYE.forEach(color => {
     ['controller', 'creative_controller', 'grid', 'crafting_grid', 'pattern_grid', 'fluid_grid', 'network_receiver', 'network_transmitter', 'relay', 'detector', 'security_manager', 'wireless_transmitter', 'disk_manipulator', 'crafter', 'crafter_manager', 'crafting_monitor'].forEach(machine => {
       event.hide(`refinedstorage:${color}_${machine}`)
