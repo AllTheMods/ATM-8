6827c6e2ff21ca4b1df00ebad4caa4b66b4022f3
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2022-12-23 01:14:47 +0800

    Update worldgen.js
    Add more ores to the mining Dim

--- kubejs/startup_scripts/worldgen.js
+++ kubejs/startup_scripts/worldgen.js
@@ -99,4 +99,81 @@ WorldgenEvents.add(event => {
     ore.squared()
     ore.uniformHeight(64, 254)
   })
+    event.addOre((ore) => {
+    ore.id = "kubejs:mining_mysticalagriculture_inferium"
+    ore.biomes = ['allthemodium:mining']
+    ore.addTarget('minecraft:stone', 'mysticalagriculture:inferium_ore')
+    ore.addTarget('minecraft:deepslate', 'mysticalagriculture:deepslate_inferium_ore')
+    ore.addTarget('minecraft:netherrack', 'mysticalagradditions:nether_inferium_ore')
+    ore.addTarget('minecraft:end_stone', 'mysticalagradditions:end_inferium_ore')
+    ore.size(7)
+    ore.count(4,8)
+    ore.squared()
+    ore.uniformHeight(-64, 254)
+  })
+  event.addOre((ore) => {
+    ore.id = "kubejs:mining_mysticalagriculture_prosperity"
+    ore.biomes = ['allthemodium:mining']
+    ore.addTarget('minecraft:stone', 'mysticalagriculture:prosperity_ore')
+    ore.addTarget('minecraft:deepslate', 'mysticalagriculture:deepslate_prosperity_ore')
+    ore.addTarget('minecraft:netherrack', 'mysticalagradditions:nether_prosperity_ore')
+    ore.addTarget('minecraft:end_stone', 'mysticalagradditions:end_prosperity_ore')
+    ore.size(7)
+    ore.count(2,6)
+    ore.squared()
+    ore.uniformHeight(-64, 254)
+  })
+  event.addOre((ore) => {
+    ore.id = "kubejs:mining_silentgear_crimson_iron"
+    ore.biomes = ['allthemodium:mining']
+    ore.addTarget('minecraft:netherrack', 'silentgear:crimson_iron_ore')
+    ore.size(7)
+    ore.count(3)
+    ore.squared()
+    ore.uniformHeight(0, 65)
+  })
+  event.addOre((ore) => {
+    ore.id = "kubejs:mining_silentgear_azure_silver"
+    ore.biomes = ['allthemodium:mining']
+    ore.addTarget('minecraft:end_stone', 'silentgear:azure_silver_ore')
+    ore.size(7)
+    ore.count(3)
+    ore.squared()
+    ore.uniformHeight(-64, 1)
+  })
+  event.addOre((ore) => {
+    ore.id = "kubejs:mining_evil_dark"
+    ore.biomes = ['allthemodium:mining']
+    ore.addTarget('minecraft:stone', 'evilcraft:dark_ore')
+    ore.addTarget('minecraft:deepslate', 'evilcraft:dark_ore_deepslate')
+    ore.size(5)
+    ore.count(2, 4)
+    ore.squared()
+    ore.uniformHeight(64, 254)
+  })
+  let Color = ['blue','red','green','light','dark']
+  Color.forEach(Color =>
+    event.addOre((ore) => {
+      ore.id = `kubejs:mining_xycraft_${Color}_ore`
+      ore.biomes = ['allthemodium:mining']
+      ore.addTarget('minecraft:stone', `xycraft_world:xychorium_ore_stone_${Color}`)
+      ore.addTarget('minecraft:deepslate', `xycraft_world:xychorium_ore_deepslate_${Color}`)
+      ore.size(7)
+      ore.count(5)
+      ore.squared()
+      ore.uniformHeight(64, 254)
+    })
+  )
+  event.addOre((ore) => {
+    ore.id = "kubejs:mining_ato_iridium"
+    ore.biomes = ['allthemodium:mining']
+    ore.addTarget('minecraft:stone', 'alltheores:iridium_ore')
+    ore.addTarget('minecraft:deepslate', 'alltheores:deepslate_iridium_ore')
+    ore.addTarget('minecraft:netherrack', 'alltheores:nether_iridium_ore')
+    ore.addTarget('minecraft:end_stone', 'alltheores:end_iridium_ore')
+    ore.size(5)
+    ore.count(1)
+    ore.squared()
+    ore.uniformHeight(-64, 254)
+  })
 })
