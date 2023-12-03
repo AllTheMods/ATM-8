6ae13352d64db63119e1ddf052266635b50a29fc
Mitchell5200 <45913837+Mitchell5200@users.noreply.github.com>
2023-12-03 19:21:18 +0800

    Update thermal.js
    move most code to ore_processing.js, fix thermal integ osmium

--- kubejs/server_scripts/mods/thermal/thermal.js
+++ kubejs/server_scripts/mods/thermal/thermal.js
@@ -4,162 +4,19 @@
 */
 
 ServerEvents.recipes(event =>{
-//AllTheOres
+// Other code moved to ore_processing
+  
+//Fix thermal Integ having the recipe set for the pulverizer instead of the induction smelter
+event.remove({ id: `thermal:compat/mekanism/smelter_mek_osmium_ore`})
 event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": `alltheores:raw_aluminum`
-    },
-    "result": [
-        {
-        "item": `alltheores:aluminum_dust`,
-        "chance": 1.25
-        },
-        {
-        "item": 'alltheores:tin_dust',
-        "chance": 0.05
-        }
+    type: "thermal:smelter",
+    ingredient: {"tag": "forge:ores/osmium"},
+    result: [
+      {item: "alltheores:osmium_ingot","chance": 1.0},
+      {item: "minecraft:copper_ingot","chance": 0.2},
+      {item: "thermal:rich_slag","chance": 0.2}
     ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_ato_aluminum`)
-
-event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": `alltheores:raw_platinum`
-    },
-    "result": [
-        {
-        "item": `alltheores:platinum_dust`,
-        "chance": 1.25
-        },
-        {
-        "item": 'alltheores:tin_dust',
-        "chance": 0.05
-        }
-    ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_ato_platinum`)
-
-event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": `alltheores:raw_uranium`
-    },
-    "result": [
-        {
-        "item": `alltheores:uranium_dust`,
-        "chance": 1.25
-        },
-        {
-        "item": 'alltheores:lead_dust',
-        "chance": 0.05
-        }
-    ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_ato_uranium`)
-
-event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": `alltheores:raw_zinc`
-    },
-    "result": [
-        {
-        "item": `alltheores:zinc_dust`,
-        "chance": 1.25
-        },
-        {
-        "item": 'alltheores:copper_dust',
-        "chance": 0.05
-        }
-    ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_ato_zinc`)
-
-event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": `alltheores:raw_iridium`
-    },
-    "result": [
-        {
-        "item": `alltheores:iridium_dust`,
-        "chance": 1.25
-        }
-    ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_ato_iridium`)
-
-event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": `alltheores:raw_osmium`
-    },
-    "result": [
-        {
-        "item": `alltheores:osmium_dust`,
-        "chance": 1.25
-        }
-    ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_ato_osmium`)
-
-//AllTheModium
-let atm_ores = ['allthemodium','unobtainium','vibranium',]
-atm_ores.forEach(atm_ores =>
-    event.custom({
-        "type": "thermal:pulverizer",
-        "ingredient": {
-          "item": `allthemodium:raw_${atm_ores}`
-        },
-        "result": [
-            {
-            "item": `allthemodium:${atm_ores}_dust`,
-            "chance": 1.25
-            },
-            {
-            "item": 'alltheores:netherite_dust',
-            "chance": 0.05
-            }
-        ],
-        "experience": 0.1
-    }).id(`kubejs:pulverizing_${atm_ores}`)
-)
-//Sgear
-event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": `silentgear:raw_azure_silver`
-    },
-    "result": [
-        {
-        "item": `silentgear:azure_silver_dust`,
-        "chance": 1.25
-        },
-        {
-        "item": 'alltheores:silver_dust',
-        "chance": 0.05
-        }
-    ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_azure_silver`)
-event.custom({
-    "type": "thermal:pulverizer",
-    "ingredient": {
-      "item": 'silentgear:raw_crimson_iron'
-    },
-    "result": [
-        {
-        "item": 'silentgear:crimson_iron_dust',
-        "chance": 1.25
-        },
-        {
-        "item": 'alltheores:iron_dust',
-        "chance": 0.05
-        }
-    ],
-    "experience": 0.1
-}).id(`kubejs:pulverizing_crimson_iron`)
+    experience: 0.2
+  }).id(`thermal:compat/mekanism/smelter_mek_osmium_ore`)
 
 })
